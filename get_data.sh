4a24e76394e1b731b2d3c31be82a2061233eba43
Gabriel <gugundi@gmail.com>
2020-03-20 09:35:14 +0100

    Fixed scenegraph and questions

--- get_data.sh
+++ get_data.sh
@@ -1,11 +1,16 @@
 # This script is for downloading the GQA dataset
 mkdir -p dataset
-cd data
+cd dataset
 
 # Get Scene Graphs
+mkdir scene_graphs
+cd scene_graphs
 wget https://nlp.stanford.edu/data/gqa/sceneGraphs.zip
 unzip sceneGraphs.zip
+cd ..
 
 # Get Questions
+mkdir questions
+cd questions
 wget https://nlp.stanford.edu/data/gqa/questions1.3.zip
 unzip questions1.3.zip
